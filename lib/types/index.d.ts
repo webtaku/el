d7c1d35b773166b001bf362a167b082271a99bd5
webtaku <webtakudev@gmail.com>
2025-08-03 11:31:26 +0900

    Add 'class' property to ElementProps type for enhanced flexibility

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -5,6 +5,7 @@ type ElementByTag<T extends Tag | string> = (T extends '' ? HTMLDivElement : (T 
 type ElementBySelector<S extends Selector> = (S extends '' ? HTMLDivElement : (S extends keyof HTMLElementTagNameMap ? HTMLElementTagNameMap[S] : (S extends `${infer T}#${string}` ? ElementByTag<T> : (S extends `${infer T}.${string}` ? ElementByTag<T> : HTMLElement))));
 type ElementProps<S extends Selector> = Partial<Omit<ElementBySelector<S>, 'style'>> & {
     style?: Partial<CSSStyleDeclaration>;
+    class?: string;
 };
 declare function el<S extends Selector>(selector?: S, ...args: (HTMLElement | string | ElementProps<S> | undefined)[]): ElementBySelector<S>;
 export { el };
